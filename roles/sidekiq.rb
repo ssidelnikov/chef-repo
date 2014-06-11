4b0dfe7695cd9c8cce266b17cf38556ef3d7d065
Stanislav Sidelnikov <sidelnikov.stanislav@gmail.com>
2014-06-11 13:00:36 +0600

    Added monit startup fix for ubuntu

--- roles/sidekiq.rb
+++ roles/sidekiq.rb
@@ -1,4 +1,4 @@
 name 'sidekiq'
 description 'This role configures a sidekiq server to run'
-run_list "recipe[redisio::install]", "recipe[redisio::enable]", "recipe[monit]", "recipe[sidekiq]"
+run_list "recipe[redisio::install]", "recipe[redisio::enable]", "recipe[monit]", "recipe[monit::ubuntu12fix]", "recipe[sidekiq]"
 
